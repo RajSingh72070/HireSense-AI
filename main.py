ac617b1736c7839bc9c4970f6c506b9f718b2858
RajSingh72070 <raj.194m.singh@gmail.com>
2026-07-07 09:28:51 +0530

    testing

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+print("1")
+
 from fastapi import FastAPI
+print("2")
 
 from app.database.database import Base, engine
+print("3")
+
+from app.database import models
+print("4")
+
+Base.metadata.create_all(bind=engine)
+print("5")
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello from Render"}
+    return {"status": "working"}
